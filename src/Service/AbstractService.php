be0bb1db256175968177ba977a8b8c43b82991ff
d00ker <d00ker@icloud.com>
2020-07-28 20:59:35 +0300

    Add decamelize method

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -24,4 +24,12 @@ abstract class AbstractService {
     public function getClient() {
         return $this->client;
     }
+
+    /**
+     * @param string $string A string for decamelizition.
+     * @return string A decamelize string.
+     */
+    public function decamelize(string $string) {
+        return strtolower(preg_replace(['/([a-z\d])([A-Z])/', '/([^_])([A-Z][a-z])/'], '$1_$2', $string));
+    }
 }
